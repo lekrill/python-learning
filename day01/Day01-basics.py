53ea75c29e7ee021060f07ee5d88668f3e7534d3
lekrill <ee.cabuena@gmail.com>
2026-09-09 16:40:17 +0000

    Day 1 Exercises Basics

---
+++ day01/Day01-basics.py
@@ -0,0 +1,30 @@
+# Variables & Printing
+# This is basic variable declaration.
+name = "Neo"
+age = 27
+print(f"Hi {name}, I am {age}")
+
+
+
+
+#Loops
+ 
+for i in range(5):
+	print(i)
+
+#Conditionals
+
+score = 70
+if score >=90:
+	print("A")
+elif score >= 80:
+	print("B")
+else:
+	print ("bagsak ka boi")
+
+# Functions 
+
+def greet(name):
+ return f"Hello, {name}!"
+
+print(greet("Aliya"))
---
+++ day01/odd-even.py
@@ -0,0 +1,37 @@
+#Variables 
+
+numA = 55
+numB = 28
+numC = 9898197
+numD = 2
+
+#Display Results
+
+print(f"Numbers Listed")
+if numA % 2 == 0:
+	print(f"{numA} Even")
+
+elif numA % 2 != 0:
+	print(f"{numA} Odd")
+
+if numB % 2 == 0:
+	print(f"{numB} Even")
+
+elif numB % 2 != 0:
+	print(f"{numB} Odd")
+
+if numC % 2 == 0:
+	print(f"{numC} Even")
+
+elif numC % 2 != 0:
+	print(f"{numC} Odd")
+
+if numD % 2 == 0:
+	print(f"{numD} Even")
+
+elif numD % 2 != 0:
+	print(f"{numD} Odd")
+
+
+
+
